b667730a3fec00336a01797d2dc82e8852fa1aa5
Alice Cretin-Anciaux <alice_ca@hotmail.fr>
2021-11-05 09:29:34 +0100

    footer

--- routes/index.js
+++ routes/index.js
@@ -83,6 +83,9 @@ router.get('/lastTrips', async function (req, res, next) {
 // check pq card marche 1 fois sur 2
 // pouvoir aller sur toutes les pages QUE si session + login/sign-in (redirect si req.session==undefined) : (rajouter un pop-up pour dire qu'on doit se logger ?)
 // clic sur connection : logout si req.session
+// gérer route confirm  
+// gestion majuscule/minuscule
+//toggle gestion
 
 
 
